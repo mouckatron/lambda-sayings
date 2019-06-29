dcbb8e1debfa33ef2f09a61867999ee64bf71d13
Graham <mouckatron@gmail.com>
2019-06-29 09:48:28 +0100

    First stab at IAM for the lambda

---
+++ terraform/iam.tf
@@ -0,0 +1,46 @@
+
+resource "aws_iam_role" "sayings" {
+  name = "lambda-sayings"
+  path = "/service-role/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "sayings" {
+  name = "lambda_sayings_policy"
+  role = "${aws_iam_role.sayings.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:DescribeStream",
+                "dynamodb:GetRecords",
+                "dynamodb:GetShardIterator",
+                "dynamodb:ListStreams",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
